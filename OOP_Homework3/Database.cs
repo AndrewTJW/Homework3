4aacff3a72fd602428c5e44863b5808cdb2425e8
AndrewTJW <130200621+AndrewTJW@users.noreply.github.com>
2024-10-23 17:23:35 +0800

    Add files via upload

---
+++ OOP_Homework3/Database.cs
@@ -0,0 +1,194 @@
+﻿using FirebaseAdmin;
+using Google.Cloud.Firestore;
+using Homework2Library;
+
+namespace OOP_Homework3
+{
+    internal class Database
+    {
+        private const string firebase_key = "homework3-bbd99";
+        private FirestoreDb database;
+        
+        //temporary lists are created to store newly retrieved data from Firebase
+        public TransactionList tmp_trans { get; set; } 
+        public Inventory tmp_items { get; set; }
+
+        public Database()
+        {
+            tmp_trans = new TransactionList();
+            tmp_items = new Inventory();
+
+        }
+
+        public void initDatabase()
+        {
+            FirebaseApp.Create();
+            database = FirestoreDb.Create(firebase_key);
+            Console.WriteLine("Created Cloud Firestore client with project ID: {0}", firebase_key);
+        }
+
+        //to save transaction
+        public async Task saveTransaction(UserTransaction a_user_transaction)
+        {
+            CollectionReference collectionRef = database.Collection("user_transactions");
+            DocumentReference docRef = collectionRef.Document(DateTime.Now.Ticks.ToString()); //DateTime.Now.Ticks.ToString() is used because it has near perfect unique ID
+            Dictionary<string, object> transaction_data = new Dictionary<string, object>
+            {
+                {"TransactionReason", a_user_transaction.getTransactionReason()},
+                {"Amount", a_user_transaction.getAmount()},
+                //need to save transaction date as string because firebase only allows UTC timestamp, if save as DateTime need to convert to UTC before saving
+                {"TransactionDate", a_user_transaction.getTransactionDateTime().ToString()}
+            };
+
+            Console.WriteLine("Transaction with ID: {0} has been saved.\n", docRef.Id);
+            Console.WriteLine("Data saved: \n");
+            Console.WriteLine("Transaction amount: {0}", a_user_transaction.getAmount());
+            Console.WriteLine("Transaction reason: {0}", a_user_transaction.getTransactionReason());
+            Console.WriteLine("Transaction date: {0}", a_user_transaction.getTransactionDateTime());
+            await docRef.SetAsync(transaction_data);
+        }
+
+        //to save user items
+        public async Task saveAsset(Item a_user_asset)
+        {
+            CollectionReference collectionRef = database.Collection("user_items");
+            DocumentReference docRef = collectionRef.Document(DateTime.Now.Ticks.ToString());
+            Dictionary<string, object> items = new Dictionary<string, object>
+            {
+                {"Item ID", a_user_asset.getItemID()},
+                {"Item Name", a_user_asset.getItemName()},
+                {"Quantity", a_user_asset.getItemCount()},
+                {"Price", a_user_asset.getPrice()}
+            };
+            Console.WriteLine("User asset with ID: {0} has been saved.\n", docRef.Id);
+            Console.WriteLine("Data saved: \n");
+            Console.WriteLine("Item Name: {0}", a_user_asset.getItemName());
+            Console.WriteLine("Item ID: {0}", a_user_asset.getItemID());
+            Console.WriteLine("Quantity: {0}", a_user_asset.getItemCount());
+            Console.WriteLine("Price: {0}", a_user_asset.getPrice());
+            await docRef.SetAsync(items);
+        }
+
+        public async Task saveUserData(UserDetail a_user_detail)
+        {
+            CollectionReference collectionRef = database.Collection("user_detail");
+            DocumentReference docRef = collectionRef.Document(a_user_detail.getUserID());
+            Dictionary<string, object> userdata = new Dictionary<string, object>
+            {
+                {"user_ID", a_user_detail.getUserID()},
+                {"email", a_user_detail.getEmail()},
+                {"password", a_user_detail.getPassword()},
+                {"name", a_user_detail.getName()},
+                {"age", a_user_detail.getAge()}
+            };
+            Console.WriteLine("User detail with email {0} saved successfully", a_user_detail.getEmail());
+            await docRef.SetAsync(userdata);
+        }
+        //to retrieve data from Firebase according to collection and it gets every document inside the collection and temporarily stores in in a dictionary
+        //it is then distributed to temporary variables to store the value 
+        //then it is transferred to a temporary object
+        public async Task retrieveTransaction()
+        {
+            Query collectionQuery = database.Collection("user_transactions");
+            QuerySnapshot allQuerySnapshot = await collectionQuery.GetSnapshotAsync();
+
+            tmp_trans.Clear();
+            foreach (DocumentSnapshot documentSnapshot in allQuerySnapshot.Documents)
+            {
+                Dictionary<string, object> data = documentSnapshot.ToDictionary();
+                double tmp_amount = double.Parse(data["Amount"].ToString());
+                //since firebase can only accept timestamps in UTC format, we need to have these additional operations to convert the UTC time back to Local Time
+                /*
+                string tmp_timestamp = data["TransactionDate"].ToString();
+                string tmp_cleanedtimestamp = tmp_timestamp.Replace("Timestamp: ", "");
+                DateTime tmp_datetime_utc = DateTime.Parse(tmp_cleanedtimestamp);
+                DateTime tmp_datetime_local = tmp_datetime_utc.ToLocalTime();
+                */ //either this solution or just save it in string then convert back to DateTime
+                DateTime tmp_date = DateTime.Parse(data["TransactionDate"].ToString());
+
+                string tmp_transreason = data["TransactionReason"].ToString();
+                                                                                                  //if use alternative solution replace with tmp_datetime_local
+                UserTransaction tmp_transaction = new UserTransaction(tmp_amount, tmp_transreason, tmp_date);
+                tmp_trans.AddTransaction(tmp_transaction);
+            }
+            Console.WriteLine("Data retrieved: \n");
+            tmp_trans.DisplayTransactionHistory();
+        }
+
+        //method to retrive user items from database
+        public async Task retrieveItem()
+        {
+            Query collectionQuery = database.Collection("user_items");
+            QuerySnapshot allQuerySnapshot = await collectionQuery.GetSnapshotAsync();
+
+            tmp_items.Clear();
+            foreach (DocumentSnapshot documentSnapshot in allQuerySnapshot.Documents)
+            {
+                Dictionary<string, object> data = documentSnapshot.ToDictionary();
+
+                string tmp_itemID = data["Item ID"].ToString();
+                string tmp_itemName = data["Item Name"].ToString();
+                int tmp_quantity = int.Parse(data["Quantity"].ToString());
+                double tmp_price = double.Parse(data["Price"].ToString());
+
+                Item tmp_item = new Item(tmp_itemName, tmp_quantity, tmp_price, tmp_itemID);
+                tmp_items.AddItem(tmp_item);
+            }
+            Console.WriteLine("Data retrived: \n");
+            tmp_items.DisplayInventory();
+        }
+
+        //retrieve single document == user detail
+        public async Task retrieveUserData(string a_user_ID)
+        {
+            //shorthand way of writing, got it from documentation.
+            DocumentReference docRef = database.Collection("user_detail").Document(a_user_ID);
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+
+            Dictionary<string, object> data = snapshot.ToDictionary();
+                string tmp_userID = data["user_ID"].ToString();
+                string tmp_email = data["email"].ToString();
+                string tmp_password = data["password"].ToString();
+                string tmp_name = data["name"].ToString();
+                int tmp_age = int.Parse(data["age"].ToString());
+            UserDetail tmp_userdetail = new UserDetail(tmp_email, tmp_password, tmp_userID, tmp_name, tmp_age);
+
+            Console.WriteLine("Data of user ID: {0} has been successfully retrieved.", tmp_userdetail.getUserID());
+            tmp_userdetail.DisplayUserData();
+        }
+
+        //delete transaction with reference to tick time AKA documentID
+        public async Task deleteTransaction(string docID)
+        {
+            CollectionReference collectionRef = database.Collection("user_transactions");
+            DocumentReference docRef = collectionRef.Document(docID);
+
+            Console.WriteLine("Transaction with ID: {0} has been deleted", docRef.Id);
+            await docRef.DeleteAsync();
+        }
+        //delete user data => usually used for updating settings
+        public async Task deleteUserData(string a_user_ID)
+        {
+            DocumentReference docRef = database.Collection("user_detail").Document(a_user_ID);
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+
+            Console.WriteLine("User data with ID: {0} has been deleted", a_user_ID);
+            await docRef.DeleteAsync();
+        }
+        //delete user item
+        public async Task deleteAsset(string docID)
+        {
+            CollectionReference collectionRef = database.Collection("user_items");
+            DocumentReference docRef = collectionRef.Document(docID);
+
+            //getting the key value pairs of the item stored in database    
+            DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+            Item tmp_item = snapshot.ConvertTo<Item>();
+
+            Console.WriteLine("Item with ID: {0} has been deleted", tmp_item.getItemID());
+            await docRef.DeleteAsync();
+        }
+
+
+    }
+}
---
+++ OOP_Homework3/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+After testing, everything works
+Time tested: 11:51PM 22th October 2024
+*/
+
+
+using Google.Cloud.Firestore;
+//using class library
+using Homework2Library;
+
+namespace OOP_Homework3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test code
+            Database database = new Database();
+            database.initDatabase();
+
+            //create a object for demonstration
+            UserTransaction transaction1 = new UserTransaction(500, "Transportation", DateTime.Now);
+
+            // create a new task to carry out the saving function
+            // need to do this way because app is responsive
+            var task1 = Task.Run(async () => await database.saveTransaction(transaction1));
+            task1.Wait(); //blocks until the task is complete
+
+            UserTransaction transaction2 = new UserTransaction(100, "Food and Beverage", DateTime.Now);
+
+            task1 = Task.Run(async () => await database.saveTransaction(transaction2));
+            task1.Wait();
+
+            UserTransaction transaction3 = new UserTransaction(300, "Advanced Salary", DateTime.Now);
+
+            task1 = Task.Run(async () => await database.saveTransaction(transaction3));
+            task1.Wait();
+
+            Item item1 = new Item("Car", 1, 56000, "1");
+
+            task1 = Task.Run(async () => await database.saveAsset(item1));
+            task1.Wait();
+
+            Item item2 = new Item("Table", 50, 25.50, "2");
+
+            task1 = Task.Run(async () => await database.saveAsset(item2));
+            task1.Wait();
+
+            //retrive data test
+            var task2 = Task.Run(async () => await database.retrieveTransaction());
+            task2.Wait();
+
+            task2 = Task.Run(async () => await database.retrieveItem());
+            task2.Wait();
+
+
+            //testing delete method => deleteAsset([docID])
+            var task4 = Task.Run(async () => await database.deleteAsset("638652376210777632"));
+            task4.Wait();
+
+            task4 = Task.Run(async() => await database.deleteAsset("638652376211282668"));
+            task4.Wait();
+
+
+
+            //testing delete method => deleteTransaction([docID])
+            var task3 = Task.Run(async () => await database.deleteTransaction("638652376205314712"));
+            task3.Wait();
+
+            task3 = Task.Run(async () => await database.deleteTransaction("638652376209496751"));
+            task3.Wait();
+
+            task3 = Task.Run(async () => await database.deleteTransaction("638652376210163143"));
+            task3.Wait();
+
+            //testing saving user detail
+
+            UserDetail user1 = new UserDetail("evelynn@gmail.com", "12345pass", "Evelynn", 21);
+
+            var task5 = Task.Run(async () => await database.saveUserData(user1));
+            task5.Wait();
+            //retrieving user data 
+            task5 = Task.Run(async () => await database.retrieveUserData("789d18ac-203a-47c5-89ea-8e9693874d59"));
+            task5.Wait();
+            //testing delete method
+            var task6 = Task.Run(async () => await database.deleteUserData("789d18ac-203a-47c5-89ea-8e9693874d59"));
+            task6.Wait();
+        }
+    }
+}
